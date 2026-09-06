991503cc6f974a48f0600d988df6b7af5d5113d4
Alfonso Ricky <alfonsorickykurnia@gmail.com>
2026-09-06 14:39:36 +0800

    Add initial Rental Linen page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rental Linen</title>
+</head>
+<body>
+    <h1>Rental Linen</h1>
+    <p>Selamat datang di aplikasi Rental Linen.</p>
+</body>
+</html>
